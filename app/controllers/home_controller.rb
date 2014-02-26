8606d21725990f0d3e60170ca567c0785e51f350
Sean Lecky <seanlecky@i-am-awesome.lan>
2014-02-26 00:00:27 +0000

    initial commit from skeleton

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+
+  def recipe
+  	# Root for @title variable, used everywhere
+  	@title = "Ripe"
+  end
+
+  def smoothie
+
+  end
+
+  def about
+  	
+  end
+
+  def contact
+  	
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recipe::Application.config.secret_key_base = '92d33aa42f4165d89cdbc581cdbad1c0492d8ee8c48292c0d962a32ce9fc2b76ff92fa53b49f84c6546fff06d97bc871e5b86206717b0d07f54c45773ce93785'
